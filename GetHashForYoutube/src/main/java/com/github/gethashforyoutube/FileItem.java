90cc9fe2037add198762a924658cfd65a3f13fb6
Anupam Gangotia <agangotia@gmail.com>
2013-10-25 11:06:23 -0400

    t2

---
+++ GetHashForYoutube/src/main/java/com/github/gethashforyoutube/FileItem.java
@@ -0,0 +1,16 @@
+package com.github.gethashforyoutube;
+
+import java.io.FileInputStream;
+
+/**
+ * @author Anupam
+ *
+ */
+public class FileItem {
+	
+	private String fileName;//File Name
+	
+	FileInputStream fisToreadFile;//This is the input stream to read file
+	
+
+}
